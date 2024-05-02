420a58c4ab9c9a857d64fe558e65375b5baa402b
kumaraswamy <care.xoma@gmail.com>
2024-05-02 14:34:48 +0530

    Delete ResultCallback.java

--- src/xyz/kumaraswamy/lin2/ResultCallback.java
+++
@@ -1,9 +0,0 @@
-package xyz.kumaraswamy.lin2;
-
-import xyz.kumaraswamy.lin2.structs.Result;
-
-import java.util.List;
-
-public interface ResultCallback {
-  void onResult(List<Result> results);
-}
